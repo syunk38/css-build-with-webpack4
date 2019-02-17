9f8e993b134efcacad662dd4c2bfc0c7c68a800f
Syun <syunk38@gmail.com>
2019-02-17 15:22:40 +0900

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: { 
+    "css/style": `${__dirname}/src/css/style.scss`, 
+    "js/bundle": `${__dirname}/src/js/index.js`
+  },
+  output: {
+    path: `${__dirname}/dist/`,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [{loader: MiniCssExtractPlugin.loader}, 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
+  ],
+};
